db61bb65af5f3d29b73a0f94586adc03810423db
jean elossy <jean.elossy@yahoo.com>
2021-08-05 04:51:47 +0200

    Initialisation du projet

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const basket = document.getElementById("basket");
+const delivery = document.getElementById("delivery");
+const confirm = document.getElementById("confirm");
+const done = document.getElementById("done");
+
+const numberProgression =document.querySelectorAll(".number__pregression");
+const barProgression =document.querySelector("#progression");
